5036c3cb97a525e660ebfbb390742af5e268625c
vichosty <vicho.yan@gmail.com>
2023-06-25 18:12:26 -0400

    creacion token usuario

--- src/server/controllers/auth.controller.js
+++ src/server/controllers/auth.controller.js
@@ -1,20 +1,36 @@
 import User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+import {createAccessToken} from '../libs/jwt.js'
 
 export const register = async (req, res) => {
     const {email, password, rut} = req.body
     try{
+
+        const  passwordHash = await bcrypt.hash(password, 10)  
+        //encriptacion de contraseña 1 metodo de seguiridad
+
         const newUser = new User({
             rut,
             email,
-            password
+            password : passwordHash,
         });
 
         const userSaved = await newUser.save();
-        res.json(userSaved)
+
+
+        const token = await createAccessToken({id : userSaved._id});
+
+        res.cookie('token', token);
+        
+        res.json({
+            id: userSaved._id,
+            rut: userSaved.rut,
+            email: userSaved.email,
+        })
 
 
     }catch(error){
-        console.log(error);
+        res.status(500).json({ message: error.message });
     }
 
     
