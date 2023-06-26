9cbd2614f6004b130b1f9a27020232233bfcbb80
Niicool1 <71039083+Niicool1@users.noreply.github.com>
2023-06-26 18:55:01 -0400

    Update Inicio.vue

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -4,23 +4,55 @@
 
 <template>
   <div class="home">
-    
-    <div class="card" style="width: 25%;">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a class="btn btn-primary">Go somewhere</a>
-    </div>
+        <ul class="row" id="imagenInicio">
+          <li class="col">
+            <div class="card" style="width: 40rem; height:30rem;">
+              <img src="../assets/images/rutinadeportiva.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Artículo sobre rutinas deportivas</h5>
+                <p class="card-text">
+                  Aspectos fundamentales para tener una buena rutina de entrenamiento
+                </p>
+                <a href="#" class="btn btn-primary">Mas información</a>
+              </div>
+            </div>
+          </li>
+          <li class="col">
+            <div class="card" style="width: 40rem; height:30rem;">
+              <img src="../assets/images/infonutricional.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Artículo sobre información nutricional</h5>
+                <p class="card-text">La importancia de conocer la información nutricional de tus alimentos</p>
+                <a href="#" class="btn btn-primary">Mas información</a>
+              </div>
+            </div>
+          </li>
+          <li class="col">
+            <div class="card" style="width: 40rem; height:30rem;">
+              <img src="../assets/images/alimentacion-saludable.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Articulo sobre alimentación</h5>
+                <p class="card-text">Consejos para tener una buena alimentación</p>
+                <a href="#" class="btn btn-primary">Mas información</a>
+              </div>
+            </div>
+          </li>
+          <li class="col">
+            <div class="card" style="width: 40rem; height:30rem;">
+              <img src="../assets/images/habitosaludable.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Articulo sobre hábitos para una vida saludable</h5>
+                <p class="card-text">La importancia de los hábitos para llevar un estilo de vida saludable</p>
+                <a href="#" class="btn btn-primary">Mas información</a>
+              </div>
+            </div>
+          </li>
+        </ul>
 
 </div>
-
-    
-  </div>
-
-
-
 </template>
 
 <style lang ="scss" scoped>
     @import '@/assets/mainstyles.scss';
 </style>
+
