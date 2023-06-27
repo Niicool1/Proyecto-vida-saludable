f85188f475b147baabf975eec75b2d6d6ac0b854
Niicool1 <71039083+Niicool1@users.noreply.github.com>
2023-06-27 00:42:30 -0400

    Update perfilUsuario.vue

--- src/views/perfilUsuario.vue
+++ src/views/perfilUsuario.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="PerfilUsuario">
-        
-        <main class="p-5">
-            <section>
-                <div class="fotoPerfil">
-                    
-                </div>
-                <ul class="Perfil">
-                    <h5>Nombre de perfil</h5>
+    <div class="container">
+        <div class="row">
+            <div class="PerfilUsuario">
+                <ul class="perfil">
+                    <li>
+                        <div class="fotoPerfil">
+                            <img src="../assets/images/perfil.png" class="rounded-circle img-thumbnail">   
+                        </div>
+                        <h5>Nombre de perfil</h5>
+                    </li>
+                       
+                    <li>
+                        <div class="border border-dark" id="dashboardPerfil">
+                            <p>Dashboard con información sobre la dieta del usuario y su actividad deportiva</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="boton">
+                            <div class="btn-group boton2" role="group" aria-label="Basic example">
+                                <button type="button" class="btn btn-light">Ingresar nueva comida</button>
+                                <button type="button" class="btn btn-light">Ingresar actividad fisica</button>
+                                <button type="button" class="btn btn-light">Información en detalle sobre mi alimentación</button>
+                            </div>
+                        </div>
+                          
+                    </li>
+                        
                 </ul>
-                
-            </section>
-            
-            <section>
-                <div class="border border-dark" id="dashboardPerfil">
-                    <p>Dashboard con información sobre la dieta del usuario y su actividad deportiva</p>
-                </div>
-                
-            </section>
-            <section>
-                <ul class="Perfil">
-                    <h5>Ingresar nueva comida</h5>
-                    <h5>Ingresar actividad fisica</h5>
-                    <h5>Información en detalle sobre mi ah5mentación</h5>
-                </ul>
-            </section>
 
-        </main>
-
+            </div>
+        </div>
     </div>
 </template>
 
