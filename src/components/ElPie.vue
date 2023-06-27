7ea5e8281b5d556e85cc26d074634aece9630958
vichosty <vicho.yan@gmail.com>
2023-06-27 05:41:40 -0400

    footer texto

--- src/components/ElPie.vue
+++ src/components/ElPie.vue
@@ -6,24 +6,25 @@
       <div class="row">
         <!--Grid column-->
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-          <h5 class="text-uppercase">Footer text</h5>
+          <h5 class="text-uppercase">ejercítate regularmente</h5>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-            aliquam voluptatem veniam, est atque cumque eum delectus sint!
+            Mantenerse activo y hacer ejercicio regularmente es esencial para una vida saludable. 
+            No solo mejora la salud física, sino que también puede mejorar la salud mental y el bienestar general. 
+            ¡Encuentra una actividad que disfrutes y hazla parte de tu rutina diaria!
           </p>
         </div>
         <!--Grid column-->
 
         <!--Grid column-->
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-          <h5 class="text-uppercase">Footer text</h5>
+          <h5 class="text-uppercase">Consume alimentos saludables</h5>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
-            aliquam voluptatem veniam, est atque cumque eum delectus sint!
+            Comer bien es fundamental para mantener una buena salud. Una dieta equilibrada y 
+            nutritiva puede ayudar a prevenir enfermedades y mejorar la calidad de vida. 
+            ¡Incluye una variedad de frutas, verduras, proteínas y 
+            granos en tu dieta para obtener todos los nutrientes que necesitas!
           </p>
         </div>
         <!--Grid column-->
