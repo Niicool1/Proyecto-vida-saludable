32c1219b3832dba26b3ca937a0c3edd718c7d275
Niicool1 <71039083+Niicool1@users.noreply.github.com>
2023-06-26 20:44:06 -0400

    Update alimentosRecomendados.vue

--- src/views/alimentosRecomendados.vue
+++ src/views/alimentosRecomendados.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="container-fluid bg-light bg-gradient">
-        <div class="container alimentosRecomendados">
+        <div class="container">
             <div id="recomendacion">
-                <a>Alimentos recomendados</a>
+                <li>Alimentos recomendados</li>
+                
             </div>
             <ul class="row" id="alimento">
                 <li class="col">
                     <div class="card" style="width: 25rem; height: 30rem;">
+                        <img src="../assets/images/alimento1.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title">Alimento</h5>
+                        <h5 class="card-title">Kiwi</h5>
                         <p class="card-text">Tus alimentos presentan una deficiencia de X nutriente por lo que te recomendamos este alimento.</p>
                         <a href="#" class="btn btn-primary">Mas información</a>
                         </div>
@@ -16,8 +18,9 @@
                 </li>
                 <li class="col">
                     <div class="card" style="width: 25rem; height: 30rem;">
+                        <img src="../assets/images/alimento2.jpeg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title">Alimento</h5>
+                        <h5 class="card-title">Palta</h5>
                         <p class="card-text">Tus alimentos presentan una deficiencia de X nutriente por lo que te recomendamos este alimento.</p>
                         <a href="#" class="btn btn-primary">Mas información</a>
                         </div>
@@ -26,8 +29,9 @@
             
                 <li class="col">
                     <div class="card" style="width: 25rem; height: 30rem;">
+                        <img src="../assets/images/alimento3.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
-                        <h5 class="card-title">Alimento</h5>
+                        <h5 class="card-title">Almendras</h5>
                         <p class="card-text">Tus alimentos presentan una deficiencia de X nutriente por lo que te recomendamos este alimento.</p>
                         <a href="#" class="btn btn-primary">Mas información</a>
                         </div>
@@ -39,6 +43,7 @@
     </div>
     
 </template>
+
 
 <style lang ="scss" scoped>
     @import '@/assets/mainstyles.scss';
